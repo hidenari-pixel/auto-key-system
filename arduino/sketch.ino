187f2740678030fc91be520fbd05350578cd863c
no-name-pixel <82199171+hidenari-pixel@users.noreply.github.com>
2022-04-17 12:47:52 +0900

    Arduino : delayを調整

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -1,38 +1,39 @@
 #include <Servo.h>
 
-
  Servo myservo;          // Servoオブジェクトの宣言
  const int SV_PIN = 7;   // サーボモーターをデジタルピン7に
- int analogPin = 3;
+ const int mosfet = 3;
  char data;
  
- void setup(){
+ void setup(){   
    Serial.begin(9600);
    myservo.attach(SV_PIN, 500, 2400);  // サーボの割当(パルス幅500~2400msに指定)
    Serial.println("Connect \n");
-   myservo.write(160);
-}
+   myservo.write(90);
+   analogWrite(mosfet, 255);
+   delay(1000);
+ }
  
  void loop(){
-  analogWrite(analogPin, 0);
+   analogWrite(mosfet, 0);
    if (Serial.available() > 0) {
     data = (char) Serial.read();
     Serial.println(data);
      if (data == '1'){
-      analogWrite(analogPin, 255);
+      analogWrite(mosfet, 255);
       Serial.println("Open");
+      delay(100); 
+      myservo.write(0);
       delay(500);
-      myservo.write(75);    // サーボモーターを0度の位置まで動かす
-        delay(1000);
-      analogWrite(analogPin, 0);
+      analogWrite(mosfet, 0);
      }
-      else if (data == '0'){
-      analogWrite(analogPin, 255);
+     else if (data == '0'){
+      analogWrite(mosfet, 255);
       Serial.println("Close");
+      delay(100);
+      myservo.write(90);
       delay(500);
-      myservo.write(160);    // サーボモーターを0度の位置まで動かす
-      delay(1000);
-      analogWrite(analogPin, 0);
+      analogWrite(mosfet, 0);
      }
    }
-}
+ }
